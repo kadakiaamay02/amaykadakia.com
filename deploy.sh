35067a75a991c4b0ea878bd5468b619cb603b70d
Amay Kadakia <kadakiaamay02@gmail.com>
2026-01-21 22:43:47 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Navigate to your project folder
-cd /home/pi/amay-portfolio
+cd /home/laezy/amaykadakia.com
 
 # 2. Pull the latest changes from GitHub
 git pull origin main
@@ -15,7 +15,7 @@ npx ng build --configuration production
 
 # 5. Clear the old web files and move the new build
 sudo rm -rf /var/www/html/*
-sudo cp -r dist/amay-portfolio/browser/* /var/www/html/
+sudo cp -r dist/amaykadakia-portfolio/* /var/www/html/
 
 # 6. Ensure Apache has correct permissions
 sudo chown -R www-data:www-data /var/www/html/
