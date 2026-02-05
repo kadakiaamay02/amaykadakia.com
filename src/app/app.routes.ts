5ea1c1b251c9bf0868cebc590173c522d88d1282
Amay Kadakia <kadakiaamay02@gmail.com>
2026-02-04 21:25:13 -0600

    rps init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { GamesMenuComponent } from './components/games-menu/games-menu.component';
+import { RockPaperScissorsComponent } from './components/rock-paper-scissors/rock-paper-scissors.component';
 
 
 export const routes: Routes = [
@@ -8,7 +9,8 @@ export const routes: Routes = [
     path: '',
     children: [
       { path: '', component: AboutComponent },
-      { path: 'games', component: GamesMenuComponent }
+      { path: 'games', component: GamesMenuComponent },
+      { path: 'games/rock-paper-scissors', component: RockPaperScissorsComponent}
     ]
   }
 ];
---
+++ src/app/components/rock-paper-scissors/rock-paper-scissors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-rock-paper-scissors',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './rock-paper-scissors.component.html',
+  styleUrl: './rock-paper-scissors.component.scss'
+})
+export class RockPaperScissorsComponent {
+
+}
