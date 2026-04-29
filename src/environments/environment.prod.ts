339387a436c979bff4c8f83e17b78b04531da510
Amay Kadakia <kadakiaamay02@gmail.com>
2026-04-28 22:53:27 -0500

    port fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   name: 'Amay Kadakia Portfolio',
   version: '2.0.0',
-  apiUrl: 'http://amaypy.duckdns.org:5000'
+  apiUrl: 'https://amaypy.duckdns.org'
 };
