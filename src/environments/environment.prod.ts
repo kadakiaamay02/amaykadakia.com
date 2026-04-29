a700904089f2849d9a885ab3db71ea57f155aead
Amay Kadakia <kadakiaamay02@gmail.com>
2026-04-28 19:05:42 -0500

    revert envirments.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 
 export const environment = {
-  production: false,
+  production: true,
   name: 'Amay Kadakia Portfolio',
   version: '2.0.0',
   apiUrl: 'http://192.168.5.153:5000'
