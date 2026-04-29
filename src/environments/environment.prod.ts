5e555171e5d6a05f8480480843fcfae3fb291e1a
Amay Kadakia <kadakiaamay02@gmail.com>
2026-04-28 22:22:58 -0500

    fix: update apiUrl to duckdns

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-
 export const environment = {
   production: true,
   name: 'Amay Kadakia Portfolio',
