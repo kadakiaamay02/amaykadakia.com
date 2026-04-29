f5334ca1f7205377d5db079c0b79d63445192403
Amay Kadakia <kadakiaamay02@gmail.com>
2026-04-28 22:15:51 -0500

    duckdns

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   production: true,
   name: 'Amay Kadakia Portfolio',
   version: '2.0.0',
-  apiUrl: 'http://130.45.125.245:5000'
+  apiUrl: 'http://amaypy.duckdns.org:5000'
 };
