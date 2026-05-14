64d1d521df3eca86502efd542331249d732c1a7a
Amay Kadakia <kadakiaamay02@gmail.com>
2026-05-14 18:25:02 -0500

    unifi

--- backend/app.py
+++ backend/app.py
@@ -171,7 +171,7 @@ def init_db():
 
 init_db()
 
-def send_email(subject, body_text, body_html, to="2400roundrock+Alerts@gmail.com"):
+def send_email(subject, body_html, to="amaykadakia+Alerts@gmail.com"):
     try:
         email_content = f"""From: laezywork@gmail.com
         To: {to}
