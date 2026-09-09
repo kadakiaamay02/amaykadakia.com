f6abdd876501121e4bf7aa3e1228b05f233c3c49
Amay Kadakia <kadakiaamay02@gmail.com>
2026-09-08 21:55:38 -0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     // ... other imports
-    HttpClientModule 
+    HttpClientModule,
+    FormsModule
   ],
 })
 export class AppModule { }
